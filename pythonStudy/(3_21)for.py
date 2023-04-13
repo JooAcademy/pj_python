55df918ab988f40ee955dc28f84c6b9e0f113608
itmaster <itmaster@gill.or.kr>
2023-04-13 13:03:24 +0900

    회사에서 수정

--- pythonStudy/(3_21)for.py
+++ pythonStudy/(3_21)for.py
@@ -1,15 +1,21 @@
-for i in range(7):
-    print(i)
+# for i in range(7):
+#     print(i)
     
     
     
     
 name_list = ['홍길동', '정다인', '김철수']    
-age_list = [500,5,12]
+#age_list = [500, 5, 12]
+
+#print(age_list)
+#print(len(age_list))
     
 for i, k in enumerate(name_list):
     print("i = ", i, end = '  ')
     print("k = ", k, end = ' 좋은 결과 있기를  ')
+    
+
+
 
 
 
